a635eaca4d8e0a45ed544e3ebe1f40c144ceb803
dokuchaev <dmitry.nsaa@gmail.com>
2016-11-28 03:53:49 +0300

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-$(".card-header").click(function(){$(".fa-accordion").removeClass("active-fa"),$(this).hasClass("collapsed")&&$(this).find(".fa-accordion").addClass("active-fa")});
+$('.card-header').click(function() {
+    $('.fa-accordion').removeClass('active-fa');
+    if ($(this).hasClass('collapsed')) {
+        $(this).find('.fa-accordion').addClass('active-fa');
+    }
+})
